f85a2bd8e30865118d743ed5212b04481cacde42
SawanPanwar <skpanwar25@gmail.com>
2024-12-24 18:08:01 +0530

    24-12-2024

--- spring-boot-test/src/main/java/com/rays/ctl/DemoCtl.java
+++ spring-boot-test/src/main/java/com/rays/ctl/DemoCtl.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -115,4 +117,10 @@ public class DemoCtl {
 		return res;
 	}
 
+	@PostMapping("submit")
+	public ORSResponse submit(@RequestBody Person person) {
+		ORSResponse res = new ORSResponse();
+		res.addData(person);
+		return res;
+	}
 }
