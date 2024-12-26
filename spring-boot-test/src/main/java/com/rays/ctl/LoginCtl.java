b8ec4c765e61897281b95247e069d02139f68a07
SawanPanwar <skpanwar25@gmail.com>
2024-12-26 18:30:48 +0530

    26-12-2024

--- spring-boot-test/src/main/java/com/rays/ctl/LoginCtl.java
+++ spring-boot-test/src/main/java/com/rays/ctl/LoginCtl.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rays.common.ORSResponse;
 import com.rays.dto.UserDTO;
+import com.rays.form.LoginForm;
 import com.rays.form.UserRegistrationForm;
 import com.rays.service.UserService;
 
@@ -35,6 +36,21 @@ public class LoginCtl {
 
 		res.addData(pk);
 		res.addMessage("User Registered Successfully..!!");
+		return res;
+	}
+
+	@PostMapping("login")
+	public ORSResponse login(@RequestBody LoginForm form) {
+
+		ORSResponse res = new ORSResponse();
+
+		UserDTO dto = service.authenticate(form.getLoginId(), form.getPassword());
+
+		if (dto != null) {
+			res.addData(dto);
+		} else {
+			res.addMessage("Login & Password invalid");
+		}
 		return res;
 	}
 }
